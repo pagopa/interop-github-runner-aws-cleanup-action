61b18d1d3ee01be84fee72e2fa5836fc68f0a16e
mmorini-dxc <151859827+mmorini-dxc@users.noreply.github.com>
2025-07-24 12:22:05 +0200

    feat: replace curl with gh cli (#6)

--- scripts/cleanupGithubRunner.sh
+++ scripts/cleanupGithubRunner.sh
@@ -2,12 +2,14 @@
 
 set -eo pipefail
 
+export GH_TOKEN="$PAT_TOKEN"
+
 RUNNER_NAME="${TARGET_ENV}-${GITHUB_RUN_ID}-${MATRIX_INDEX}"
 
-GITHUB_RUNNER_ID=$(curl -s \
+GITHUB_RUNNER_ID=$(gh api --paginate \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${PAT_TOKEN}" \
-  https://api.github.com/repos/${GITHUB_REPO}/actions/runners \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  /repos/${GITHUB_REPO}/actions/runners \
   | jq '.runners | map(select(.name == "'${RUNNER_NAME}'")) | .[].id' -r)
 
 ECS_TASK_ID=""
@@ -16,10 +18,11 @@ if [[ -n "${ECS_TASK_ID_ENV}" ]]; then
   ECS_TASK_ID=${ECS_TASK_ID_ENV}
   echo "[INFO] Use ECS Task id from artifact ${ECS_TASK_ID_ENV}"
 else
-  ECS_TASK_ID=$(curl -s \
+
+  ECS_TASK_ID=$(gh api --paginate \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer ${PAT_TOKEN}" \
-    https://api.github.com/repos/${GITHUB_REPO}/actions/runners/${GITHUB_RUNNER_ID} \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    /repos/${GITHUB_REPO}/actions/runners/${GITHUB_RUNNER_ID} \
     | jq -r '.labels[] | select(.name | startswith("task_id:")) | .name' \
     | cut -d: -f2)
 fi
@@ -30,21 +33,21 @@ aws ecs stop-task \
 
 echo "[INFO] ECS task ${ECS_TASK_ID} stopped"
 
-curl -s \
-  -X DELETE \
+gh api \
+  --method DELETE \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${PAT_TOKEN}" \
-  https://api.github.com/repos/${GITHUB_REPO}/actions/runners/${GITHUB_RUNNER_ID}
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  /repos/${GITHUB_REPO}/actions/runners/${GITHUB_RUNNER_ID}
 
 START_TIME=$(date +%s)
 while [ $(( $(date +%s) - 120 )) -lt $START_TIME ]; do
 
   echo "[INFO] Waiting for runner ${RUNNER_NAME} to be deleted from Github"
 
-  GITHUB_RUNNER_ID=$(curl -s \
+  GITHUB_RUNNER_ID=$(gh api --paginate \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer ${PAT_TOKEN}" \
-    https://api.github.com/repos/${GITHUB_REPO}/actions/runners \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    /repos/${GITHUB_REPO}/actions/runners \
     | jq '.runners | map(select(.name == "'${RUNNER_NAME}'")) | .[].id' -r)
 
   if [ -z "$GITHUB_RUNNER_ID" ]; then
